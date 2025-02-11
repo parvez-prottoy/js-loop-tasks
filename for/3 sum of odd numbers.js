908307d836665248998701bea0d18684924273a1
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-11 11:50:09 +0600

    added for sum of event or odd task

--- for/3 sum of odd numbers.js
+++ for/3 sum of odd numbers.js
@@ -1,5 +1,3 @@
-
-
 /***
 
 Subtask-1:
@@ -16,3 +14,21 @@ Display sum of all the even numbers from 51 to 85.
  */
 
 /*programming hero*/
+
+// Subtask-1:
+let sumOddNumbers = 0;
+for (let i = 91; i <= 129; i++) {
+  if (i % 2 !== 0) {
+    sumOddNumbers += i;
+  }
+}
+console.log(`Total Odd Sum : ${sumOddNumbers}`);
+
+// Subtask-2:
+let sumEvenNumbers = 0;
+for (let i = 51; i <= 85; i++) {
+  if (i % 2 === 0) {
+    sumEvenNumbers += i;
+  }
+}
+console.log(`Total Even Sum : ${sumEvenNumbers}`);
