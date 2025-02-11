82aa0cf8a5b0887680f4d595f35cbcd9915c8845
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-11 12:41:38 +0600

    added while count down  task

--- while/5 countdown.js
+++ while/5 countdown.js
@@ -4,5 +4,12 @@ Implement a countdown timer that counts down from 21 to 15.
 
  */
 
-
 /*programming hero*/
+
+let i = 81;
+while (i >= 65) {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000);
+  i--;
+}
