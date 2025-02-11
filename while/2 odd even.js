3491961b32a95fe72fa8dc39d1bed502375ae213
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-11 12:35:07 +0600

    added while odd & event task

--- while/2 odd even.js
+++ while/2 odd even.js
@@ -6,7 +6,6 @@ Find all the odd numbers from 61 to 100.
 
  */
 
-
 /***
 
 Subtask-2:
@@ -16,3 +15,21 @@ Find all the even numbers from 78 to 98.
  */
 
 /*programming hero*/
+
+// Subtask-1:
+let o = 61;
+while (o <= 100) {
+  if (o % 2 !== 0) {
+    console.log(o);
+  }
+  o++;
+}
+
+// Subtask-2:
+let e = 78;
+while (e <= 98) {
+  if (e % 2 === 0) {
+    console.log(e);
+  }
+  e++;
+}
