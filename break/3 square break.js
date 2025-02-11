e8160e43a668c1708c4e62d0af4f904ab4403413
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-11 11:29:24 +0600

    added break square task

--- break/3 square break.js
+++ break/3 square break.js
@@ -1,3 +1,11 @@
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+let firstSquare;
+for (let i = 1; i <= 100; i++) {
+  if (Math.sqrt(i)) {
+    firstSquare = i;
+    break;
+  }
+}
+console.log(firstSquare);
