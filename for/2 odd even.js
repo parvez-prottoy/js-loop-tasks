cc66dcb961365662da5e2063de601026193b6202
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-11 11:42:24 +0600

    added for odd & even task

--- for/2 odd even.js
+++ for/2 odd even.js
@@ -6,7 +6,6 @@ Find all the odd numbers from 61 to 100.
 
  */
 
-
 /***
 
 Subtask-2:
@@ -16,3 +15,17 @@ Find all the even numbers from 78 to 98.
  */
 
 /*programming hero*/
+
+// Subtask-1:
+for (let i = 61; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+// Subtask-2:
+for (let i = 78; i <= 98; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
